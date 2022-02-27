1ab264fea0ef3b2f70f19e84fd6765e97bc6834f
Ethan Ray <ethan.gregory.ray@gmail.com>
2022-02-27 16:27:11 +0000

    Rider comments

--- ecm1410_coursework/src/cycling/Rider.java
+++ ecm1410_coursework/src/cycling/Rider.java
@@ -16,6 +16,13 @@ public class Rider {
     private String name;
     private int yearOfBirth;
     
+
+    /**
+     * @param riderId int - A unique Id for each rider is given 
+     * @param teamID int - A team Id that the rider will belong too
+     * @param name String - A name for the rider, Has to be non-null or IllegalArgumentException is thrown.
+     * @param yearOfBirth int - A year that the rider was born in. Has to be above 1900 or IllegalArgumentException is thrown.
+     */
     public Rider(int riderId,int teamID, String name, int yearOfBirth)throws IllegalArgumentException
     {
         this.riderId = riderId;
@@ -24,17 +31,32 @@ public class Rider {
             throw new IllegalArgumentException("Illegal name entered for rider");
         }
         this.name = name;
+        if (yearOfBirth < 1900){
+            throw new IllegalArgumentException("Illegal value for yearOfBirth given please enter a value above 1900.");
+        }
         this.yearOfBirth = yearOfBirth;
     }
+    /**
+     * @return The RiderId of the rider.
+     */
     public int getRiderId(){
         return this.riderId;
     }
+    /**
+     * @return The team Id that the rider belongs to/
+     */
     public int getRiderTeamId(){
         return this.teamID;
     }
+    /**
+     * @return The rider's name.
+     */
     public String getRiderName(){
         return this.name;
     }
+    /**
+     * @return The the year of birth of the rider.
+     */
     public int getRiderYOB(){
         return this.yearOfBirth;
     }
