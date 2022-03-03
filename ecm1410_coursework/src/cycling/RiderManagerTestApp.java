c7d221164fd4f5c106cc4aff1d503144a6ba7ee6
Ethan Ray <ethan.gregory.ray@gmail.com>
2022-03-03 14:09:33 +0000

    Testing and renaming

---
+++ ecm1410_coursework/src/cycling/RiderManagerTestApp.java
@@ -0,0 +1,29 @@
+package cycling;
+import java.util.Arrays;
+
+public class RiderManagerTestApp {
+    public static void main(String[] args) throws IllegalNameException,InvalidNameException,IDNotRecognisedException{
+        RiderManager myRiderManager = new RiderManager();
+        int testTeamId=myRiderManager.createTeam("EthansTeam", "Ethans Team for Racing!!!");
+        int testTeamId2=myRiderManager.createTeam("EthansTeam2", "Ethans Team2 for Racing!!!");
+        myRiderManager.createRider(testTeamId, "Ethan", 2003);
+        myRiderManager.createRider(testTeamId, "Thomas", 2002);
+        printInfo(myRiderManager,testTeamId);
+        //int testTeamId2=myRiderManager.createTeam("EthansTeam", "Ethans Team for Racing!!!"); // Throws IllegalName Exception as expected
+        myRiderManager.removeRider(0);
+        printInfo(myRiderManager,testTeamId);
+        myRiderManager.createRider(testTeamId, "Ethan", 2003);
+        printInfo(myRiderManager,testTeamId);
+        System.out.println(Arrays.toString(myRiderManager.getTeams()));
+        
+
+
+    }
+    public static void printInfo(RiderManager myRiderManager,int testTeamId) throws IDNotRecognisedException{
+        System.out.println("-----------------------------------------------");
+        System.out.println("RiderNames:"+Arrays.toString(myRiderManager.getRidersNames()));
+        System.out.println("Id of Riders in teamId "+testTeamId+":"+Arrays.toString(myRiderManager.getTeamRiders(testTeamId)));
+        System.out.println("TeamNames:"+Arrays.toString(myRiderManager.getTeamsNames()));
+        System.out.println("Id of Teams:"+Arrays.toString(myRiderManager.getTeams()));
+    }
+}
