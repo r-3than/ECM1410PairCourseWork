6b2df8eb9e712b766cf07d21ffe2330f2358b885
Ethan Ray <ethan.gregory.ray@gmail.com>
2022-03-03 11:46:18 +0000

    commenting in RiderManager

--- ecm1410_coursework/src/cycling/RiderManager.java
+++ ecm1410_coursework/src/cycling/RiderManager.java
@@ -6,15 +6,24 @@ public class RiderManager {
     private static ArrayList<Rider> allRiders = new ArrayList<Rider>();
     private static ArrayList<Team> allTeams = new ArrayList<Team>();
     
+
+    /**
+     * @param teamID int - A team Id that the rider will belong too. If the ID doesn't exist IDNotRecognisedException is thrown.
+     * @param name String - A name for the rider, Has to be non-null or IllegalArgumentException is thrown.
+     * @param yearOfBirth int - A year that the rider was born in. Has to be above 1900 or IllegalArgumentException is thrown.
+     * @return riderId of the rider created.
+     */
     int createRider(int teamID, String name, int yearOfBirth) throws IDNotRecognisedException,IllegalArgumentException{
         int teamIndex = getIndexForTeamId(teamID);
-
         Rider newRider = new Rider(teamID,name,yearOfBirth);
         allRiders.add(newRider);
         Team ridersTeam = allTeams.get(teamIndex);
         ridersTeam.addRider(newRider);
         return newRider.getRiderId();
     }
+    /**
+     * @param riderId int - A riderId of a rider to be removed. If the ID doesn't exist IDNotRecognisedException is thrown.
+     */
     void removeRider(int riderId) throws IDNotRecognisedException
     {
         int riderIndex = getIndexForRiderId(riderId);
