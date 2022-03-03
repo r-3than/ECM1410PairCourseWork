a3ae9cab9fe6b81d7e768f44385d082ce597cb4e
tom-newbold <thomas_newbold@outlook.com>
2022-03-03 12:35:21 +0000

    adds stage to race

--- ecm1410_coursework/src/cycling/RaceTestApp.java
+++ ecm1410_coursework/src/cycling/RaceTestApp.java
@@ -1,9 +1,13 @@
 package cycling;
 
+import java.time.LocalDateTime;
+
 public class RaceTestApp {
     public static void main(String[] args) throws IllegalNameException,
-                            InvalidNameException, IDNotRecognisedException {
-        Race r1 = new Race("France", "Monaco Race");
+                            InvalidNameException, InvalidLengthException,
+                            IDNotRecognisedException {
+        Race r1 = new Race("France", "Tour de France");
+        r1.addStageToRace("Monaco", "Road Race", 50.1, LocalDateTime.of(2022, 1, 9, 13, 0), StageType.FLAT);
         r1.toString();
         int[] stageIDs = r1.getStages();
         for(int id : stageIDs) {
