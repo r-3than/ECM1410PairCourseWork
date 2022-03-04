97a53a7e6b887a708bde6c060d0ba88a170fe094
tom-newbold <thomas_newbold@outlook.com>
2022-03-04 17:51:04 +0000

    remove segment tests

--- ecm1410_coursework/src/cycling/RaceTestApp.java
+++ ecm1410_coursework/src/cycling/RaceTestApp.java
@@ -11,25 +11,26 @@ public class RaceTestApp {
         r1.addStageToRace("Monaco", "Road Race", 10.0, LocalDateTime.of(2022, 1, 9, 13, 0), StageType.TT);
         r1.addStageToRace("Nice", "Mountain Road", 25.0, LocalDateTime.of(2022, 1, 11, 9, 0), StageType.HIGH_MOUNTAIN);
         int[] stageIDs = r1.getStages();
-        Stage s2 = Stage.getStage(stageIDs[1]);
-        s2.addSegmentToStage(5.0, SegmentType.SPRINT, 1.0, 5.0);
-        s2.addSegmentToStage(15.0, SegmentType.C3, 4.5, 10.0);
-        s2.addSegmentToStage(21.0, SegmentType.C1, 8.0, 6.0);
-        s2.addSegmentToStage(25.0, SegmentType.C2, 10.0, 4.0);
-        int[] segmentIds = s2.getSegments();
+        Stage s1 = Stage.getStage(stageIDs[1]);
+        s1.addSegmentToStage(5.0, SegmentType.SPRINT, 1.0, 5.0);
+        s1.addSegmentToStage(15.0, SegmentType.C3, 4.5, 10.0);
+        s1.addSegmentToStage(21.0, SegmentType.C1, 8.0, 6.0);
+        s1.addSegmentToStage(25.0, SegmentType.C2, 10.0, 4.0);
+        int[] segmentIds = s1.getSegments();
         // instance states
         System.out.println(r1.toString());
         System.out.println(Stage.getStage(stageIDs[0]).toString());
-        System.out.println(s2.toString());
+        System.out.println(s1.toString());
         for(int id : segmentIds) {
             System.out.println(Segment.getSegment(segmentIds[id]));
         }
         Race.removeStage(0);
-        s2 = Stage.getStage(stageIDs[0]);
-        segmentIds = s2.getSegments();
+        s1 = Stage.getStage(stageIDs[0]);
+        Stage.removeSegment(3);
+        segmentIds = s1.getSegments();
         // instance states
         System.out.println(r1.toString());
-        System.out.println(s2.toString());
+        System.out.println(s1.toString());
         for(int id : segmentIds) {
             System.out.println(Segment.getSegment(segmentIds[id]));
         }
