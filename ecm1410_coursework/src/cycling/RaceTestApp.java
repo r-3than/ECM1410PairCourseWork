b01de215c66d9ae957a27aa6751bcc2f88c7a692
tom-newbold <thomas_newbold@outlook.com>
2022-03-04 15:02:26 +0000

    improved

--- ecm1410_coursework/src/cycling/RaceTestApp.java
+++ ecm1410_coursework/src/cycling/RaceTestApp.java
@@ -8,14 +8,20 @@ public class RaceTestApp {
                             IDNotRecognisedException, InvalidStageStateException,
                             InvalidStageTypeException, InvalidLocationException {
         Race r1 = new Race("France", "Tour de France");
-        r1.addStageToRace("Monaco", "Road Race", 50.1, LocalDateTime.of(2022, 1, 9, 13, 0), StageType.HIGH_MOUNTAIN);
+        r1.addStageToRace("Monaco", "Road Race", 10.0, LocalDateTime.of(2022, 1, 9, 13, 0), StageType.TT);
+        r1.addStageToRace("Nice", "Mountain Road", 25.0, LocalDateTime.of(2022, 1, 11, 9, 0), StageType.HIGH_MOUNTAIN);
         int[] stageIDs = r1.getStages();
-        Stage s1 = Stage.getStage(stageIDs[0]);
-        s1.addSegmentToStage(16.7, SegmentType.C1, 12.0, 16.7);
-        int[] segmentIds = s1.getSegments();
+        Stage s2 = Stage.getStage(stageIDs[1]);
+        s2.addSegmentToStage(5.0, SegmentType.SPRINT, 1.0, 5.0);
+        s2.addSegmentToStage(15.0, SegmentType.C3, 4.5, 10.0);
+        s2.addSegmentToStage(21.0, SegmentType.C1, 8.0, 6.0);
+        s2.addSegmentToStage(25.0, SegmentType.C2, 10.0, 4.0);
+        int[] segmentIds = s2.getSegments();
         // instance states
         System.out.println(r1.toString());
-        System.out.println(s1.toString());
-        System.out.println(Segment.getSegment(segmentIds[0]));
+        System.out.println(s2.toString());
+        for(int id : segmentIds) {
+            System.out.println(Segment.getSegment(segmentIds[id]));
+        }
     }
 }
