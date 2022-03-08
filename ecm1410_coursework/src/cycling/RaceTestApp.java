d1732e15da6c4bf5dff5327c0fa0a53d646b62d9
tom-newbold <thomas_newbold@outlook.com>
2022-03-08 16:46:40 +0000

    fixed test

--- ecm1410_coursework/src/cycling/RaceTestApp.java
+++ ecm1410_coursework/src/cycling/RaceTestApp.java
@@ -7,6 +7,7 @@ public class RaceTestApp {
                             InvalidNameException, InvalidLengthException,
                             IDNotRecognisedException, InvalidStageStateException,
                             InvalidStageTypeException, InvalidLocationException {
+        System.out.println("instanstiating...\n");               
         Race r1 = new Race("France", "Tour de France");
         r1.addStageToRace("Monaco", "Road Race", 10.0, LocalDateTime.of(2022, 1, 9, 13, 0), StageType.TT);
         r1.addStageToRace("Nice", "Mountain Road", 25.0, LocalDateTime.of(2022, 1, 11, 9, 0), StageType.HIGH_MOUNTAIN);
@@ -24,8 +25,9 @@ public class RaceTestApp {
         for(int id : segmentIds) {
             System.out.println(Segment.getSegment(segmentIds[id]));
         }
+        System.out.println("\nremoving instances...  [stage 0; segment 3]\n");
         Race.removeStage(0);
-        s1 = Stage.getStage(stageIDs[0]);
+        s1 = Stage.getStage(r1.getStages()[0]);
         Stage.removeSegment(3);
         segmentIds = s1.getSegments();
         // instance states
